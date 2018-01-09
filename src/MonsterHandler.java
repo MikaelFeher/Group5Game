c30281bb2832b4589caf883fe79250f50f20d5d0
MikaelFeher <mikael.feher@gmail.com>
2018-01-09 11:09:49 +0100

    Game loop finished (#4)
    * Adds gameloop
    
    * Changes framecollision to NOT kill player
    Adds functionality to quit the game with awsome message :)
    
    * WIP - Handling the creation/rendering of monsters
    
    * Adds init method to gameloop
    Refactors getplayerposition method
    WIP - Adds monsterhandler class to handle the monsters
    Adds method for creating and rendering the initial 3 monsters
    
    * Adds monster movement handler
    changes the player frame detection
    
    * Basic game logic finished
    Playermovement
    Monstermovement
    Player vs monster collision detection
    Game reset and quit functionality
    
    * Updates gitignore
    
    * GameArea done (#1)
    
    * adds a player class with movement methods
    
    * WIP - frameCollisionDetection
    
    *  adds gameArea
    
    * adds reset method to player
    
    * Monster class created
    
    * MonsterSpeed adjusted to 1
    
    * Updates playerspeed from 1 to 2
    
    * Fixed merged conflicts
    adds setters for player x and y
    
    * Implements the game area class and refactors the code to work with it
    
    * Adds restructoring of the code and adds comments

---
+++ src/MonsterHandler.java
@@ -0,0 +1,88 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.util.LinkedList;
+
+public class MonsterHandler {
+    public LinkedList<Monster> monsters = new LinkedList<>();
+
+    private Monster tempMonster;
+
+     public boolean collisionDetectionMonsterVsPlayer(Player player) {
+         int pX = player.getPlayerX();
+         int pY = player.getPlayerY();
+         int mX, mY;
+
+         for (int i = 0; i < monsters.size(); i++) {
+             tempMonster = monsters.get(i);
+             mX = tempMonster.getMonsterX();
+             mY = tempMonster.getMonsterY();
+
+             if(pX == mX && pY == mY){
+                 return true;
+             }
+         }
+         return false;
+     }
+
+     public void handleMovement(Player player) {
+         int pX = player.getPlayerX();
+         int pY = player.getPlayerY();
+
+        for (int i = 0; i < monsters.size(); i++) {
+            tempMonster = monsters.get(i);
+            int diffX = getPositionDifference(pX, tempMonster.getMonsterX());
+            int diffY = getPositionDifference(pY, tempMonster.getMonsterY());
+
+            if (diffX == 0 && diffY < 0){
+                tempMonster.moveUp();
+            } else if(diffX == 0 && diffY > 0){
+                tempMonster.moveDown();
+            } else if(diffY == 0 && diffX < 0) {
+                tempMonster.moveLeft();
+            } else if(diffY == 0 && diffX > 0) {
+                tempMonster.moveRight();
+            } else {
+                if (diffX < 0 && diffY < 0) {
+                    tempMonster.moveLeft();
+                    tempMonster.moveUp();
+                } else if (diffX < 0 && diffY > 0) {
+                    tempMonster.moveLeft();
+                    tempMonster.moveDown();
+                } else if (diffX > 0 && diffY < 0) {
+                    tempMonster.moveRight();
+                    tempMonster.moveUp();
+                } else {
+                    tempMonster.moveRight();
+                    tempMonster.moveDown();
+                }
+            }
+        }
+     }
+
+     private int getPositionDifference(int playerPos, int monsterPos) {
+         return playerPos - monsterPos;
+     }
+
+    public void addMonster(Monster monster, Player player) {
+        if (monster.getMonsterX() != player.getPlayerX() && monster.getMonsterY() != player.getPlayerY()){
+            this.monsters.add(monster);
+        }
+    }
+
+    public void deleteAllMonsters(){
+        this.monsters.clear();
+    }
+
+    public int numberOfMonstersInTheList() {
+        return this.monsters.size();
+    }
+
+    public void renderMonsters(Screen screen) {
+        for (int i = 0; i < monsters.size(); i++) {
+            tempMonster = monsters.get(i);
+            screen.putString(tempMonster.getMonsterX(), tempMonster.getMonsterY(), "M", Terminal.Color.RED, Terminal.Color.BLACK);
+        }
+        screen.refresh();
+    }
+}
